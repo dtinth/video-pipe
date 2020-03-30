2392ec00a19df39c07b35ae31b95b34787260475
Glitch (midirtc) <dtinth@spacet.me>
2020-03-30 11:51:58 +0000

    🌪🍥 Checkpoint ./index.html:577339/1202 ./script.js:577339/3598

--- script.js
+++ script.js
@@ -1,117 +1,40 @@
 /* global Peer, Vue */
 
-Vue.component('port-selector', {
-  template: '#port-selector',
-  props: ['available', 'selected'],
-  computed: {
-    options() {
-      return [
-        { key: null, text: 'None selected', selected: this.selected === null },
-        ...this.available.map(item => ({
-          key: item.key,
-          text: item.name,
-          selected: this.selected === item.key
-        }))
-      ]
-    }
-  },
-  methods: {
-    select(option) {
-      this.$emit('select', option.key)
-    }
-  }
-})
-
 const app = new Vue({
   el: '#app',
   data: {
     baseUrl: location.href.replace(/[\?#].*/, ''),
     peerId: '',
     activeConnections: [],
+    mode: 'loading',
     error: null,
-    selectedInput: null,
-    selectedOutput: null,
-    availableInputs: [],
-    availableOutputs: [],
-    currentInputPort: null,
-    currentOutputPort: null,
-    received: 0,
-    sent: 0,
   },
   computed: {
     url() {
       return this.baseUrl + '#' + this.peerId
     },
-    currentInput() {
-      return this.selectedInput
-    },
-    currentOutput() {
-      return this.selectedOutput
-    },
-  },
-  watch: {
-    selectedInput: {
-      handler(nextValue, currentValue) {
-        this.currentInputPort = nextValue != null ? this.midiAccess.inputs.get(nextValue) : null
-        console.log('currentInputPort =>', this.currentInputPort)
-      },
-      immediate: true
-    },
-    selectedOutput: {
-      handler(nextValue, currentValue) {
-        this.currentOutputPort = nextValue != null ? this.midiAccess.outputs.get(nextValue) : null
-        console.log('currentOutputPort =>', this.currentOutputPort)
-      },
-      immediate: true
-    },
-    currentInputPort: {
-      handler(nextValue, currentValue) {
-        if (currentValue) {
-          currentValue.onmidimessage = () => {}
-        }
-        if (nextValue) {
-          nextValue.onmidimessage = (e) => {
-            this.sent ++
-            this.activeConnections.forEach(c => c.send([...e.data]))
-          }
-        }
-      },
-      immediate: true
-    },
   },
   async mounted() {
     const peer = new Peer(sessionStorage.savedPeerId || undefined)
     window.peer = this.peer = peer
-    const registerConnection = (conn) => {
-      this.activeConnections.push(conn)
-      conn.on('data', (data) => {
-        this.received ++
-        console.log('Received ' + data)
-        if (this.currentOutputPort) {
-          this.currentOutputPort.send(data)
-        }
-      })
-      conn.on('close', () => {
-        this.activeConnections = this.activeConnections.filter(c => c !== conn)
-      })
-    }
     peer.on('open', (id) => {
       this.peerId = id
       sessionStorage.savedPeerId = id
       const connectMatch = location.hash.match(/#(\w+)/)
       if (connectMatch) {
+        this.mode = 'receiver'
         const target = connectMatch[1]
         console.log('Connecting to ', target)
         const conn = peer.connect(target)
         conn.on('open', () => {
           console.log('Connected!')
-          registerConnection(conn)
         })
+      } else {
+        this.mode = 'receiver'
       }
     })
-    peer.on('connection', conn => {
-      console.log('Connection received!')
-      registerConnection(conn)
+    peer.on('call', conn => {
+      console.log('Call received!')
     })
     try {
       const access = this.midiAccess = await navigator.requestMIDIAccess({ sysex: false })
